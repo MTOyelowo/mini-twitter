2b3200764b4d982afd8fcf56ee470bea73461d01
M.T. Oyelowo <oyelowomayowa@gmail.com>
2023-05-11 17:06:36 +0100

    Create New Tweet Page

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,6 +1,8 @@
-import { StyleSheet, View, FlatList } from "react-native";
+import { StyleSheet, View, FlatList, Pressable } from "react-native";
 import Tweet from "../../components/Tweet";
 import tweets from "../../assets/data/tweets";
+import { Entypo } from "@expo/vector-icons";
+import { Link } from "expo-router";
 
 export default function TabOneScreen() {
   return (
@@ -9,6 +11,14 @@ export default function TabOneScreen() {
         data={tweets}
         renderItem={({ item }) => <Tweet tweet={item} />}
       />
+      <Link href="/new-tweet" asChild>
+        <Entypo
+          name="plus"
+          size={24}
+          color="white"
+          style={styles.floatingButton}
+        />
+      </Link>
     </View>
   );
 }
@@ -17,5 +27,24 @@ const styles = StyleSheet.create({
   page: {
     flex: 1,
     backgroundColor: "white",
+  },
+  floatingButton: {
+    backgroundColor: "#1C9BF0",
+    borderRadius: 50,
+    padding: 20,
+
+    position: "absolute",
+    right: 15,
+    bottom: 15,
+
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+
+    elevation: 5,
   },
 });
