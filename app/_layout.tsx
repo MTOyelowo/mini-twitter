9d4725b345d4d7fb8d3f64aa9ead770962f3fc1a
M.T. Oyelowo <oyelowomayowa@gmail.com>
2023-05-11 22:43:27 +0100

    Basic Feeds and Tweet screens with drawer navigator

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -47,9 +47,7 @@ function RootLayoutNav() {
       <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
         <Stack>
           <Stack.Screen name="(drawer)" options={{ headerShown: false }} />
-          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
           <Stack.Screen name="modal" options={{ presentation: "modal" }} />
-          <Stack.Screen name="tweet/[id]" options={{ title: "Tweet" }} />
           <Stack.Screen
             name="new-tweet"
             options={{ title: "New Tweet", headerShown: false }}
